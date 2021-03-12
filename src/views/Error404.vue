c1d51c23268728b083c24b03f254faa28ff64952
linre-90 <juho.lindemark@gmail.com>
2021-03-12 16:41:26 +0200

    added legacy url and 404 error page

---
+++ src/views/Error404.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ $t("errorInRoute") }}</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+    margin: 100px 0 100px 0;
+}
+</style>

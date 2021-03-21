ec6bef1359061b7f88049bd63ac2a080df6e735d
linre-90 <juho.lindemark@gmail.com>
2021-03-21 11:49:36 +0200

    removed preload for image

--- src/components/navBar/Navbar.vue
+++ src/components/navBar/Navbar.vue
@@ -1,47 +1,78 @@
 <template>
     <div class="navSection">
-    <div v-on:click="showNHide" id="hamburgerDiv">
-        <i class="fas fa-bars"></i>
-    </div>
-     <div name="menuitem" class="col-4" id="navLanguageSel">
-        <button class="langBtn" type="submit" @click="$i18n.locale = 'fi'"><img value="fi" class="langSelect" src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/finland.png" alt="Suomeksi" srcset=""></button>
-        <button class="langBtn" type="submit" @click="$i18n.locale = 'en'"><img value="en" class="langSelect" src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/united-kingdom.png" alt="In english" srcset=""></button>
-    </div>
-    <div class="col-4" id="catNav">
-        <img id="logoImage" src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/webProfileIMG.jpg" alt="Blogger's image as logo">
-    </div>
-    <div name="menuitem" class="col-4" id="navLinks">
-        <router-link class="link" to="/">{{$t("pages.home")}} <i class="fas fa-check-circle helper"></i></router-link>
-        <router-link class="link" to="/blog">{{$t("pages.blog")}} <i class="fas fa-check-circle helper"></i></router-link>
-        <router-link class="link" to="/contact">{{$t("pages.contact")}} <i class="fas fa-check-circle helper"></i></router-link>
-        <router-link class="link" to="/appz">{{$t("pages.appz")}} <i class="fas fa-check-circle helper"></i></router-link>
-    </div>   
+        <div v-on:click="showNHide" id="hamburgerDiv">
+            <i class="fas fa-bars"></i>
+        </div>
+        <div name="menuitem" class="col-4" id="navLanguageSel">
+            <button class="langBtn" type="submit" @click="$i18n.locale = 'fi'">
+                <img
+                    value="fi"
+                    class="langSelect"
+                    src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/finland.png"
+                    alt="Suomeksi"
+                    srcset=""
+                />
+            </button>
+            <button class="langBtn" type="submit" @click="$i18n.locale = 'en'">
+                <img
+                    value="en"
+                    class="langSelect"
+                    src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/united-kingdom.png"
+                    alt="In english"
+                    srcset=""
+                />
+            </button>
+        </div>
+        <div class="col-4" id="catNav">
+            <img
+                id="logoImage"
+                src="https://catcastimgs.s3.eu-north-1.amazonaws.com/staticimg/webProfileIMG.jpg"
+                alt="Blogger's image as logo"
+            />
+        </div>
+        <div name="menuitem" class="col-4" id="navLinks">
+            <router-link class="link" to="/"
+                >{{ $t("pages.home") }}
+                <i class="fas fa-check-circle helper"></i
+            ></router-link>
+            <router-link class="link" to="/blog"
+                >{{ $t("pages.blog") }}
+                <i class="fas fa-check-circle helper"></i
+            ></router-link>
+            <router-link class="link" to="/contact"
+                >{{ $t("pages.contact") }}
+                <i class="fas fa-check-circle helper"></i
+            ></router-link>
+            <router-link class="link" to="/appz"
+                >{{ $t("pages.appz") }}
+                <i class="fas fa-check-circle helper"></i
+            ></router-link>
+        </div>
     </div>
 </template>
 
 <script>
-
 export default {
     name: "Navbar",
-    methods:{ 
-        showNHide: () =>{
+    methods: {
+        showNHide: () => {
             const menuItems = document.getElementsByName("menuitem");
-            menuItems.forEach(item => {
-            if(item.style.display == ""){
-                item.style.display = "flex";
-                item.classList.remove("hideMenuAnimation");
-                item.classList.add("showMenuAnimation")
-            }else if(item.style.display == "flex"){
-                item.classList.add("hideMenuAnimation")
-                item.classList.remove("showMenuAnimation");
-                setTimeout(() => {
-                    item.style.display = "";
-                },1000)
-            }
-        });
-    }
- }
-}
+            menuItems.forEach((item) => {
+                if (item.style.display == "") {
+                    item.style.display = "flex";
+                    item.classList.remove("hideMenuAnimation");
+                    item.classList.add("showMenuAnimation");
+                } else if (item.style.display == "flex") {
+                    item.classList.add("hideMenuAnimation");
+                    item.classList.remove("showMenuAnimation");
+                    setTimeout(() => {
+                        item.style.display = "";
+                    }, 1000);
+                }
+            });
+        },
+    },
+};
 </script>
 
 <style scoped>
@@ -52,28 +83,28 @@ export default {
     --strongColor: #67595E;
 */
 
-.helper{
+.helper {
     display: none;
 }
- .router-link-exact-active .helper{
+.router-link-exact-active .helper {
     display: inline-block;
 }
 
-.router-link-exact-active{
-    background-color: #E8B4B8;
+.router-link-exact-active {
+    background-color: #e8b4b8;
     font-weight: bold;
 }
 
-.langBtn{
+.langBtn {
     background: none;
     border: none;
     outline: none;
 }
-.langBtn::selection{
+.langBtn::selection {
     background: none;
     border: none;
 }
-.langBtn :hover{
+.langBtn :hover {
     cursor: pointer;
     animation: highLightLanguage forwards;
     animation-duration: 0.5s;
@@ -81,36 +112,41 @@ export default {
 }
 
 @keyframes highLightLanguage {
-    from{transform:scale(1)}
-    to{transform: scale(1.2)}
+    from {
+        transform: scale(1);
+    }
+    to {
+        transform: scale(1.2);
+    }
 }
 
-*{
+* {
     box-sizing: border-box;
 }
 
-.hide{
-    display:none
+.hide {
+    display: none;
 }
 
-.col-4 {width: 33.33%;}
+.col-4 {
+    width: 33.33%;
+}
 
-#logoImage{
+#logoImage {
     height: 75px;
     width: auto;
     border-radius: 50%;
     object-fit: cover;
 }
 
-.langSelect{
+.langSelect {
     width: 2rem;
     margin-left: 1rem;
     /*Remove fowwolwing after translation*/
 }
 
-
-.navSection{
-    background-color: #EED6D3;
+.navSection {
+    background-color: #eed6d3;
     padding: 10px;
     padding-top: 2px;
     padding-bottom: 2px;
@@ -120,102 +156,98 @@ export default {
     overflow: hidden;
 }
 
-.link{
+.link {
     padding-left: 15px;
     padding-right: 15px;
     padding-top: 50%;
     padding-bottom: 50%;
     text-decoration: none;
-    color: #67595E;
+    color: #67595e;
     font-size: 1.25rem;
     letter-spacing: 2px;
     font-family: "Lato", "sans-serif";
 }
 .link:hover {
-    background-color: #E8B4B8;
-    color: #67595E;
+    background-color: #e8b4b8;
+    color: #67595e;
     cursor: pointer;
-
 }
 
-.navItemPagePointer{
+.navItemPagePointer {
     font-weight: bold;
-    background-color: #E8B4B8;
+    background-color: #e8b4b8;
 }
 
-#navLinks{
+#navLinks {
     text-align: right;
 }
-#catNav{
+#catNav {
     text-align: center;
-} 
+}
 
-#hamburgerDiv{
+#hamburgerDiv {
     display: none;
 }
 
 @media only screen and (max-width: 1250px) {
-
-    #catNav{
-        width:16.66%
+    #catNav {
+        width: 16.66%;
     }
 
-    #navLanguageSel, #navLinks{
+    #navLanguageSel,
+    #navLinks {
         width: 41.66%;
     }
 }
 
 @media only screen and (max-width: 1050px) {
-
-    .navSection{
+    .navSection {
         justify-content: center;
     }
 }
 
 @media only screen and (max-width: 980px) {
-    #navLanguageSel{
+    #navLanguageSel {
         width: 20%;
     }
 
-    #navLinks{
+    #navLinks {
         width: 80%;
     }
 
-    #logoImage{
+    #logoImage {
         display: none;
     }
-    
 }
 
 @media only screen and (max-width: 680px) {
-
-    .navSection{
-        display:flex;
+    .navSection {
+        display: flex;
         flex-direction: row;
         width: 100%;
         justify-content: flex-end;
         align-items: flex-start;
-        flex-wrap: wrap;  
+        flex-wrap: wrap;
     }
 
-    #navLinks, #navLanguageSel{
+    #navLinks,
+    #navLanguageSel {
         width: 100%;
         display: flex;
         flex-direction: column;
-
     }
 
-    #navLanguageSel{
+    #navLanguageSel {
         align-items: flex-end;
     }
 
-    #catNav{
+    #catNav {
         text-align: center;
         margin: auto;
         width: 100%;
     }
 
-    .langSelect{
+    .langSelect {
         margin-right: 30px;
         margin-top: 15px;
         margin-bottom: 15px;
@@ -228,44 +260,56 @@ export default {
         letter-spacing: 2px;
     }
 
-    #hamburgerDiv{
+    #hamburgerDiv {
         display: block;
         margin-bottom: 3rem;
         margin-right: 30px;
         margin-top: 2rem;
     }
 
-    #hamburgerDiv i{
+    #hamburgerDiv i {
         font-size: 3rem;
     }
 
-    #navLanguageSel, #navLinks{
+    #navLanguageSel,
+    #navLinks {
         display: none;
     }
 }
 
 @keyframes showMenu {
-    from {transform: scale(0); opacity: 0;}
-    to {transform: scale(1); opacity: 1;}
+    from {
+        transform: scale(0);
+        opacity: 0;
+    }
+    to {
+        transform: scale(1);
+        opacity: 1;
+    }
 }
-  
-  /* The element to apply the animation to */
-  .showMenuAnimation {
+
+/* The element to apply the animation to */
+.showMenuAnimation {
     animation-name: showMenu;
     animation-duration: 1s;
     animation-iteration-count: 1;
-  }
+}
 
-  @keyframes hideMenu {
-    from {transform: scale(1);opacity: 1;}
-    to {transform: scale(0);opacity: 0;}
-  }
-  
-  /* The element to apply the animation to */
-  .hideMenuAnimation {
+@keyframes hideMenu {
+    from {
+        transform: scale(1);
+        opacity: 1;
+    }
+    to {
+        transform: scale(0);
+        opacity: 0;
+    }
+}
+
+/* The element to apply the animation to */
+.hideMenuAnimation {
     animation-name: hideMenu;
     animation-duration: 1s;
     animation-iteration-count: 1;
-    }
-
+}
 </style>
